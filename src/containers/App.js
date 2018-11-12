5c2cbab8e3055f5c6ec01a732d11b0b203a8cf41
bela-dev <fori-prg@pathwwway.com>
2018-11-12 13:47:00 +0200

    13:46

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -20,9 +21,7 @@ class App extends Component {
     }
 
     onSearchChange = (event) => {
-        this.setState({
-            searchfield: event.target.value
-        });
+        this.setState({ searchfield: event.target.value });
     }
 
     render() {
@@ -38,7 +37,9 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />    
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />        
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
