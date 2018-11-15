073cc319c236112102bfa2beb4f0daa047a2c62a
bela-dev <fori-prg@pathwwway.com>
2018-11-15 16:51:39 +0200

    Updates

---
+++ precache-manifest.983520ad1dc7fe1ab11684cb02988c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae6b16fdee5a1161d057",
+    "url": "/robofriends/static/css/main.66816af4.chunk.css"
+  },
+  {
+    "revision": "ae6b16fdee5a1161d057",
+    "url": "/robofriends/static/js/main.ae6b16fd.chunk.js"
+  },
+  {
+    "revision": "d2410c3102bf83b3dec4",
+    "url": "/robofriends/static/css/1.84555964.chunk.css"
+  },
+  {
+    "revision": "d2410c3102bf83b3dec4",
+    "url": "/robofriends/static/js/1.d2410c31.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ccfd1735ef8415bf0b14cd8ff72e80be",
+    "url": "/robofriends/index.html"
+  }
+];
