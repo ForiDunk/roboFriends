f35c6f5fb05127f1309d5e7b826c7a1cc752784a
bela-dev <fori-prg@pathwwway.com>
2018-11-15 15:06:11 +0200

    build folder

---
+++ build/precache-manifest.1221031da16f1afd5fae0daf80cff2ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e764c9af5129aabf92",
+    "url": "/static/css/main.b630bc7b.chunk.css"
+  },
+  {
+    "revision": "35e764c9af5129aabf92",
+    "url": "/static/js/main.35e764c9.chunk.js"
+  },
+  {
+    "revision": "d2410c3102bf83b3dec4",
+    "url": "/static/css/1.84555964.chunk.css"
+  },
+  {
+    "revision": "d2410c3102bf83b3dec4",
+    "url": "/static/js/1.d2410c31.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "19c2fafcd3a6a0caecc0c75b28ea5845",
+    "url": "/index.html"
+  }
+];
